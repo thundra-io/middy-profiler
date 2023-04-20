d124ac7bc7c506b5da39a358fe92b2ce5ecbe618
Andrea Culot <andclt@amazon.com>
2023-04-20 12:19:06 +0200

    Upgrade to AWS SDK V3

--- src/reporter.js
+++ src/reporter.js
@@ -1,3 +1,5 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3"); 
+
 let s3Client
 
 module.exports.reportToS3 = async function (
@@ -9,13 +11,15 @@ module.exports.reportToS3 = async function (
     awsRequestId
 ) {
     if (!s3Client) {
-        const S3 = require('aws-sdk/clients/s3')
-        s3Client = new S3()
+        s3Client = new S3Client();
     }
+
     const params = {
         Body: JSON.stringify(profilingData),
         Bucket: bucketName,
         Key: `${pathPrefix}${functionName}/${awsRequestId}/${fileName}.cpuprofile`,
     }
-    return s3Client.putObject(params).promise()
+    const putObjectCommand = new PutObjectCommand(params);
+    
+    return await s3Client.send(putObjectCommand)
 }
