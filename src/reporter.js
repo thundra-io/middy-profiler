2b4db66727a691cc5e5728d1718155d78571ced6
Andrea Culot <andclt@amazon.com>
2023-04-21 13:04:56 +0200

    Do not cache PutObjectCommand

--- src/reporter.js
+++ src/reporter.js
@@ -1,5 +1,4 @@
 let s3Client
-let putObjectCommand
 
 module.exports.reportToS3 = async function (
     profilingData,
@@ -15,24 +14,33 @@ module.exports.reportToS3 = async function (
         Key: `${pathPrefix}${functionName}/${awsRequestId}/${fileName}.cpuprofile`,
     }
 
-    if (!s3Client) {
-        if (isSDKV3()) {
-            const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3'); 
-            s3Client = new S3Client();
-            putObjectCommand = new PutObjectCommand(params);
-        } else {
-            const S3 = require('aws-sdk/clients/s3');
-            s3Client = new S3();
-        }
-    }
-
     return isSDKV3()
-        ? s3Client.send(putObjectCommand)
-        : s3Client.putObject(params).promise();
+        ? reportWithSDKV3(params)
+        : reportWithSDKV2(params);
 }
 
 // Starting from Node18, Lambda includes JS SDK V3
 function isSDKV3() {
     const nodeVersion = process.versions.node.split('.')[0];
     return nodeVersion >= 18;
+}
+
+function reportWithSDKV3(params) {
+    const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3'); 
+
+    if (!s3Client) {    
+        s3Client = new S3Client();
+    }
+
+    const putObjectCommand = new PutObjectCommand(params);
+    return s3Client.send(putObjectCommand);
+}
+
+function reportWithSDKV2(params) {
+    if (!s3Client) {    
+        const S3 = require('aws-sdk/clients/s3');
+        s3Client = new S3();
+    }
+
+    return s3Client.putObject(params).promise();   
 }
