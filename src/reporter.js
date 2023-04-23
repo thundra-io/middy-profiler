bd3997d28cdfd03c34d9cdfe94244118d0f8a7ee
Andrea Culot <95755271+andclt@users.noreply.github.com>
2023-04-23 14:37:25 +0200

    Upgrade to AWS SDK V3 (#12)
    * Upgrade to AWS SDK V3
    
    * Keep JS SDK V2 support
    
    * Do not cache PutObjectCommand

--- src/reporter.js
+++ src/reporter.js
@@ -8,14 +8,39 @@ module.exports.reportToS3 = async function (
     functionName,
     awsRequestId
 ) {
-    if (!s3Client) {
-        const S3 = require('aws-sdk/clients/s3')
-        s3Client = new S3()
-    }
     const params = {
         Body: JSON.stringify(profilingData),
         Bucket: bucketName,
         Key: `${pathPrefix}${functionName}/${awsRequestId}/${fileName}.cpuprofile`,
     }
-    return s3Client.putObject(params).promise()
+
+    return isSDKV3()
+        ? reportWithSDKV3(params)
+        : reportWithSDKV2(params);
+}
+
+// Starting from Node18, Lambda includes JS SDK V3
+function isSDKV3() {
+    const nodeVersion = process.versions.node.split('.')[0];
+    return nodeVersion >= 18;
+}
+
+function reportWithSDKV3(params) {
+    const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3'); 
+
+    if (!s3Client) {    
+        s3Client = new S3Client();
+    }
+
+    const putObjectCommand = new PutObjectCommand(params);
+    return s3Client.send(putObjectCommand);
+}
+
+function reportWithSDKV2(params) {
+    if (!s3Client) {    
+        const S3 = require('aws-sdk/clients/s3');
+        s3Client = new S3();
+    }
+
+    return s3Client.putObject(params).promise();   
 }
