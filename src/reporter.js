f9ec8ab09e04772b1550b891195dc4bc00305a18
GitHub Action <action@github.com>
2023-04-23 12:40:06 +0000

    Release 1.0.11

--- src/reporter.js
+++ src/reporter.js
@@ -14,33 +14,31 @@ module.exports.reportToS3 = async function (
         Key: `${pathPrefix}${functionName}/${awsRequestId}/${fileName}.cpuprofile`,
     }
 
-    return isSDKV3()
-        ? reportWithSDKV3(params)
-        : reportWithSDKV2(params);
+    return isSDKV3() ? reportWithSDKV3(params) : reportWithSDKV2(params)
 }
 
 // Starting from Node18, Lambda includes JS SDK V3
 function isSDKV3() {
-    const nodeVersion = process.versions.node.split('.')[0];
-    return nodeVersion >= 18;
+    const nodeVersion = process.versions.node.split('.')[0]
+    return nodeVersion >= 18
 }
 
 function reportWithSDKV3(params) {
-    const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3'); 
+    const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
 
-    if (!s3Client) {    
-        s3Client = new S3Client();
+    if (!s3Client) {
+        s3Client = new S3Client()
     }
 
-    const putObjectCommand = new PutObjectCommand(params);
-    return s3Client.send(putObjectCommand);
+    const putObjectCommand = new PutObjectCommand(params)
+    return s3Client.send(putObjectCommand)
 }
 
 function reportWithSDKV2(params) {
-    if (!s3Client) {    
-        const S3 = require('aws-sdk/clients/s3');
-        s3Client = new S3();
+    if (!s3Client) {
+        const S3 = require('aws-sdk/clients/s3')
+        s3Client = new S3()
     }
 
-    return s3Client.putObject(params).promise();   
+    return s3Client.putObject(params).promise()
 }
